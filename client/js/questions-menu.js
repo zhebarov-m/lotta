02f959004011e2874ba68c745cb43323fbb2e160
xarcho.ev <xarcho.ev@gmail.com>
2024-04-14 15:46:18 +0300

    поправил путь

--- client/js/questions-menu.js
+++ client/js/questions-menu.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const faqItems = document.querySelectorAll(".faq__item");
 
     faqItems.forEach(function(item) {
-        const icon = item.querySelector(".icon"); // Перемещаем объявление переменной icon сюда
+        const icon = item.querySelector(".icon");
         item.addEventListener("click", function() {
             const description = this.querySelector(".faq__item_desc");
             if (description.classList.contains("show")) {
@@ -10,17 +10,15 @@ document.addEventListener("DOMContentLoaded", function() {
                 icon.textContent = "+";
                 this.classList.remove("active");
             } else {
-                // Сначала скрываем все описания и удаляем класс active у всех элементов
                 faqItems.forEach(function(item) {
                     const desc = item.querySelector(".faq__item_desc");
-                    const icon = item.querySelector(".icon"); // Перемещаем объявление переменной icon сюда
+                    const icon = item.querySelector(".icon");
                     if (desc.classList.contains("show")) {
                         desc.classList.remove("show");
                         icon.textContent = "+";
                         item.classList.remove("active");
                     }
                 });
-                // Затем показываем описание текущего элемента и добавляем класс active
                 description.classList.add("show");
                 icon.textContent = "-";
                 this.classList.add("active");
